31997b73186510c02d20ef67b0f2386ca1e448d0
Scrub10000 <shiftyninja55@gmail.com>
2017-02-09 17:40:19 -0500

    initial commit, helper class included
    a blank WPF app with a simple helper class involved.

---
+++ LinksBeHere/LinkFinder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LinksBeHere
+{
+    class LinkFinder
+    {
+        public static void findLinks()
+        {
+            string chatLogPath = @"C:\Users\wwstudent\Desktop\skypeChatHistory_8Feb17.txt";
+
+            StreamReader chatLogSR = new StreamReader(chatLogPath);
+            StreamWriter linkFinder = new StreamWriter("C:\\Users\\wwstudent\\Desktop\\skypeLinks.rtf");
+            Regex urlFinder = new Regex(@"(ftp | https ?)://[^\s]+", RegexOptions.IgnoreCase);
+            MatchCollection links = urlFinder.Matches(chatLogSR.ReadToEnd());
+            foreach (Match match in links)
+            {
+                Console.WriteLine(match);
+                linkFinder.WriteLine(match);
+            }
+        }
+    }
+}
