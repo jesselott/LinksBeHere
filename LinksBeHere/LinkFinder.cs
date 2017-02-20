ceddf1989b199b3cd723880442e2e84bf7477a60
Scrub10000 <shiftyninja55@gmail.com>
2017-02-20 14:49:15 -0500

    Added new window
    The links will now be displayed in a new window instead of in another
    text file. This is to format the links accordingly.

--- LinksBeHere/LinkFinder.cs
+++ LinksBeHere/LinkFinder.cs
@@ -27,7 +27,6 @@ namespace LinksBeHere
             }
         }
 
-       
         public string LinkWritePath
         {
             get
@@ -40,19 +39,21 @@ namespace LinksBeHere
             }
         }
 
+        // list to store the matches
+        public List<string> listOfLinks = new List<string>();
+
         // Method(s)
         public void FindLinks()
         {
             StreamReader textReader = new StreamReader(linkReadPath);
-            StreamWriter linkFinder = new StreamWriter(linkWritePath);
+            //StreamWriter linkFinder = new StreamWriter(linkWritePath);
             
 
             Regex urlFinder = new Regex(@"((https?|ftp|file)\://|www.)[A-Za-z0-9\.\-]+(/[A-Za-z0-9\?\&\=;\+!'\(\)\*\-\._~%]*)*", RegexOptions.IgnoreCase);
             MatchCollection links = urlFinder.Matches(textReader.ReadToEnd());
             foreach (Match match in links)
             {
-                linkFinder.WriteLine(match);
-                linkFinder.Flush();
+                listOfLinks.Add(match.ToString());
             }
             
         }
